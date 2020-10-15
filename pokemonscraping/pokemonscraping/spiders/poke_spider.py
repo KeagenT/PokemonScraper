a3604be149c6bfc4cc9aaf06cce0df4f012b13ce
KeagenT <49502992+KeagenT@users.noreply.github.com>
2020-10-15 03:52:18 -0400

    Added updated weight parsing logic

--- pokemonscraping/pokemonscraping/spiders/poke_spider.py
+++ pokemonscraping/pokemonscraping/spiders/poke_spider.py
@@ -1,7 +1,7 @@
 from scrapy import Spider
 from scrapy import Request
 from scrapy.selector import Selector
-from ..items import PokemonscrapingItem
+from pokemonscraping.items import PokemonscrapingItem
 
 
 class PokeSpider(Spider):
@@ -9,13 +9,27 @@ class PokeSpider(Spider):
     allowed_domains = ["serebii.net"]
     start_urls = ["https://serebii.net/pokedex-swsh/grookey/"]
 
+    def weightParse(weightSelector):
+        extractedLbs = weightSelector.extract()[0]
+        if "/" in extractedLbs:
+            extractedLbs = extractedLbs.split("/")[0].strip().replace("lbs", "")
+            return extractedLbs
+        else:
+            extractedLbs = extractedLbs.strip().replace("lbs", "")
+            return extractedLbs
+
+
     def parse(self, response):
         DexTables = Selector(response).xpath('//table[@class="dextable"]')
         AllMoves = Selector(response).xpath('//table[@class="dextable"]//td[@class="fooinfo"]//a[contains(@href,"attackdex")]//text()').extract()
         AbilitiesList = response.xpath('//table[@class="dextable"]//td[@class="fooinfo"]//a[contains(@href, "abilitydex")]//text()').extract()
+       #AllTypes = response.xpath('//table[@class="dextable"]//td[@class="cen"]//a//img[contains(@alt, "type")]/@alt').extract()
+       #CleanTypes = [type.replace("-type","") for type in AllTypes]
         item = PokemonscrapingItem()
         item['name'] = response.xpath('//table[@class="dextable"][2]//td[@class="fooinfo"]/text()').extract()[0]
         item['number'] = response.xpath('//table[@class="dextable"]//td[@class="fooinfo"]//td/text()').extract()[9]
+        item['weight'] = weightParse(response.xpath('//table[@class="dextable"]//td[contains(text(), "lbs")]/text()'))
+        item['types'] = []
         item['abilities'] = []
         item['moves'] = []
         for move in AllMoves:
