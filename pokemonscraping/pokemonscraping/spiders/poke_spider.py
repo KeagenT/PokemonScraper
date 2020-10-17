361bc148999fdb6652f676e41e16492b1a5fdaea
KeagenT <49502992+KeagenT@users.noreply.github.com>
2020-10-17 01:05:23 -0400

    + logic for the types dictionary for types item

--- pokemonscraping/pokemonscraping/spiders/poke_spider.py
+++ pokemonscraping/pokemonscraping/spiders/poke_spider.py
@@ -2,6 +2,7 @@ from scrapy import Spider
 from scrapy import Request
 from scrapy.selector import Selector
 from pokemonscraping.items import PokemonscrapingItem
+import re
 
 
 class PokeSpider(Spider):
@@ -21,17 +22,62 @@ class PokeSpider(Spider):
                 extractedLbs = extractedLbs.strip().replace("lbs", "")
                 return extractedLbs
 
-        
+        #takes the attribute list of a pokemon's type images and removes the -type for every item in the list
+        def getCleanTypes(inputTypesList):
+            return [types.replace("-type","") for types in inputTypesList]
+
+        #checks if the pokemon's type category has multiple forms, and gets a cleaned list of those forms
+        def getAlternateFormsList():
+            #Filters out junk \t \r \anything tags and empty spaces in the list
+            junkText = re.compile(r"((\\.)+|\s)")
+            dirtyAlternateFormsList = response.xpath('//table[@class="dextable"]//td[@class="cen"]//tr[*]//text()').extract()
+            cleanedForms = [form for form in dirtyAlternateFormsList if not junkText.match(form)]
+            return cleanedForms
+
+        #gets the xpath object for a given alternate form. Used when a pokemon has multiple forms with different types.
+        def getFormXPathObject(form):
+            formXpath = response.xpath('//table[@class="dextable"]//td[@class="cen"]//tr[*[contains(text(), {})]]'.format("\""+form+"\""))
+            return formXpath
+
+        #gets a list of a pokemon's type from the above function's xpath object. 
+        #Darmanitan is the only pokemon that has two different forms with the same name and thus returns 2 xpath objects
+        def getTypesFromXpath(inputXpath, index = 0):
+            return getCleanTypes(inputXpath[index].xpath('.//a//img[contains(@alt, "type")]/@alt').extract())
+
+        #builds a dictionary for the types field with multiple keys if there's multiple forms or just "normal" if there are no alternate forms for that pokemon
+        def buildTypesDict():
+            typesDict = {}
+            formsList = getAlternateFormsList()
+            #if the alternate forms list is empty
+            if not formsList:
+                NormalFormTypes = response.xpath('//table[@class="dextable"]//td[@class="cen"]//a//img[contains(@alt, "type")]/@alt').extract()
+                CleanTypes = getCleanTypes(NormalFormTypes)
+                typesDict['Normal'] = CleanTypes
+                return typesDict
+            else:
+                for alternateForm in formsList:
+                    #Literally just a check for Darmanitan
+                    currentFormXpath = getFormXPathObject(alternateForm)
+                    if alternateForm not in typesDict.keys():
+                        typesDict[alternateForm] = getTypesFromXpath(currentFormXpath)
+                    else:
+                        alternateForm = "Galarian "+ alternateForm
+                        typesDict[alternateForm] = getTypesFromXpath(currentFormXpath, 1)
+                return typesDict
+                    
+
+
+
+
+        typesDict = buildTypesDict()
         DexTables = Selector(response).xpath('//table[@class="dextable"]')
         AllMoves = Selector(response).xpath('//table[@class="dextable"]//td[@class="fooinfo"]//a[contains(@href,"attackdex")]//text()').extract()
         AbilitiesList = response.xpath('//table[@class="dextable"]//td[@class="fooinfo"]//a[contains(@href, "abilitydex")]//text()').extract()
-       #AllTypes = response.xpath('//table[@class="dextable"]//td[@class="cen"]//a//img[contains(@alt, "type")]/@alt').extract()
-       #CleanTypes = [type.replace("-type","") for type in AllTypes]
         item = PokemonscrapingItem()
         item['name'] = response.xpath('//table[@class="dextable"][2]//td[@class="fooinfo"]/text()').extract()[0]
         item['number'] = response.xpath('//table[@class="dextable"]//td[@class="fooinfo"]//td/text()').extract()[9]
         item['weight'] = weightParse(response.xpath('//table[@class="dextable"]//td[contains(text(), "lbs")]/text()'))
-        item['types'] = []
+        item['types'] = typesDict
         item['abilities'] = []
         item['moves'] = []
         for move in AllMoves:
